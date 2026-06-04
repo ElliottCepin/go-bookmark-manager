4e0ce363e058489f4a0e9dfd7fbd6bdeb0efe24e
ElliottCepin <elliottcepin5@gmail.com>
2026-06-04 11:13:30 -0700

    implementation written, untested

--- internal/store/store.go
+++ internal/store/store.go
@@ -8,6 +8,12 @@ import (
 
 type SQLiteStore struct {
 	db *sql.DB
+	createBM *sql.Stmt
+	createTag *sql.Stmt
+	createBMTag *sql.Stmt
+	deleteBM *sql.Stmt
+	getBM *sql.Stmt
+	filterByTag *sql.Stmt
 }
 
 func NewSQLiteStore(filename string) (*SQLiteStore, error) {
@@ -20,7 +26,41 @@ func NewSQLiteStore(filename string) (*SQLiteStore, error) {
 		return nil, err
 	}
 	
+	// this seems silly since concurrency is safe
+	s.db.SetMaxOpenConns(1)
+	
 	if err := s.db.Ping(); err != nil {
+		return nil, err
+	} 
+
+	_, err = s.db.Exec(`CREATE TABLE IF NOT EXISTS bookmarks (
+		id INTEGER PRIMARY KEY,
+		url TEXT NOT NULL,
+		title TEXT,
+		created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+	);`)
+
+	if (err != nil) {
+		return nil, err
+	}
+	
+	_, err = s.db.Exec(`CREATE TABLE IF NOT EXISTS tags (
+		id INTEGER PRIMARY KEY,
+		name UNIQUE TEXT NOT NULL
+	);`)
+
+	if (err != nil) {
+		return nil, err
+	}
+
+	_, err = s.db.Exec(`CREATE TABLE IF NOT EXISTS bookmark_tags (
+		bm_id INTEGER NOT NULL,
+		tag_id INTEGER NOT NULL
+		PRIMARY KEY (bm_id, tag_id)
+	);`)
+
+	if (err != nil) {
 		return nil, err
 	}
 
@@ -28,15 +68,71 @@ func NewSQLiteStore(filename string) (*SQLiteStore, error) {
 }
 
 func (s *SQLiteStore) CreateTag(name string) (int, error) {
-	return nil	
+	return -1, nil
 }
 
 func (s *SQLiteStore) createBookmarkTag(tagId int, bookmarkId int) error {
 	return nil
 }
 
-func (s *SQLiteStore) CreateBookmark(b *Bookmark) error {
-	return nil
+func (s *SQLiteStore) CreateBookmark(url string, title string, tags []string) (*domain.Bookmark, error) {
+	// check that tag exists then create tag
+	tagIds := make([]int)
+	tagNames := make([]string)
+
+	var id int
+	var name string
+
+	for _, tag := range tags {
+		row := s.db.QueryRow("SELECT id, name FROM tags where name=(?)", tag)
+
+
+		err := row.Scan(&id, &name)
+
+		if (err != null) {
+			return nil, err
+		}
+		
+		
+		if (slices.contains(tagNames) {
+			return nil, errors.New("Tags violates unique constraint: too many tags")
+		}
+		
+		tagIds := append(tagIds, id)
+		tagNames := append(tagNames, name)
+	}
+
+	r, err := s.db.Exec("INSERT INTO bookmarks (url, title) VALUES (?, ?)", url, title)
+
+	if (err != nil) {
+		return nil, err
+	}
+
+	id, err = r.LastInsertId()
+	
+	if (err != nil) {
+		return nil, err
+	}
+	
+	for _, tagId := range tagIds {
+		_, err := s.db.Exec("INSERT INTO bookmark_tags (bm_id, tag_id) VALUES (?, ?)", id, tagId)
+
+		if (err != nil) {
+			return nil, err
+		}
+	}
+
+	bm := &domain.Bookmark{
+		Id: id,
+		URL: url,
+		Title: title,
+		Tags: tagNames
+		Time: time.Now()
+	}
+
+	// finally, rewrite with hot queries
+	// do all of these need to have a context?
+	return bm, nil
 }
 
 func (s *SQLiteStore) DeleteBookmark(bookmarkId int) error {
@@ -47,6 +143,6 @@ func (s *SQLiteStore) FilterByBookmarkTag(tagId int) error {
 	return nil
 }
 
-func (s *SQLiteStore) GetBookmark(bookmarkId int) (string, []string, error) {
-	return nil
+func (s *SQLiteStore) GetBookmark(bookmarkId int) (*domain.Bookmark, error) {
+	return nil, nil
 }
--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"fmt"
 	"path/filepath"
+	"slices"
 )
 
 var count int = 0
@@ -23,16 +24,37 @@ func TestConnectDB(t *testing.T) {
 
 func TestRoundTrip(t *testing.T) {
 	s, err := NewSQLiteStore(filepath.Join(t.TempDir(), fmt.Sprintf("db-%v", readCount())))
-	
+		
 	if (err != nil) {
 		t.Errorf("Error during database setup: %v", err)
 	}
 
-	bm, err = s.CreateBookmark("https://test.site/", "Test Bookmark", []string{"Test", "Bookmark"})
+	bm, err := s.CreateBookmark("https://test.site/", "Test Bookmark", []string{"Test", "Bookmark"})
 
 	if (err != nil) {
 		t.Errorf("Error creating bookmark: %v", err)
 	}
 	
-	n, t, err := s.GetBookmark(bm)
+	bm2, err := s.GetBookmark(bm.Id)
+
+	if (err != nil) {
+		t.Errorf("Error retrieving Bookmark")
+	}
+
+	if (bm2.Id != bm.Id) {
+		t.Errorf("Original id '%v' does not match returned id '%v'", bm.Id, bm2.Id)	
+	}
+
+	if (bm.URL != bm2.URL) {
+		t.Errorf("Original url '%v' des not match returned url '%v'", bm.URL, bm2.URL)
+	}
+
+	if (bm.Title != bm2.Title) {
+		t.Errorf("Original title '%v' does not match returned title '%v'", bm.Title, bm2.Title)	
+	}
+
+	if (!slices.Equal(bm.Tags, bm2.Tags)) {
+		t.Errorf("Original tags '%v' do not match returned tags '%v'", bm.Tags, bm2.Tags)	
+	}
+	
 }
